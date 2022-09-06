3587de94e65508c0fed8521352d137737808d0f3
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-09-05 18:25:24 -0700

    Add text input area

---
+++ app.js
@@ -0,0 +1,4 @@
+// Initialize tooltips before they can be used
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
